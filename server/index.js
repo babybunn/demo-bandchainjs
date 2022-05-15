b32efe802f53aac688b47bf092762996c56682eb
babybunn <wpakasame@gmail.com>
2022-05-15 11:37:07 +0700

    add cors

--- server/index.js
+++ server/index.js
@@ -1,48 +1,50 @@
-const path = require("path");
-const express = require("express");
-const fs = require("fs");
-const exec = require("child_process").exec;
-var bodyParser = require("body-parser");
+const path = require('path')
+const express = require('express')
+const fs = require('fs')
+const cors = require('cors')
+const exec = require('child_process').exec
+var bodyParser = require('body-parser')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const app = express();
+const app = express()
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.get("/compile", (req, res) => {
-  res.json({ message: "Hello GET Compile" });
-});
+app.get('/compile', (req, res) => {
+  res.json({ message: 'Hello GET Compile' })
+})
 
-app.post("/compile", (req, res) => {
-  const code = req.body.code;
-  fs.writeFile("src/lib.rs", code, function (err) {
-    if (err) throw err;
+app.post('/compile', (req, res) => {
+  const code = req.body.code
+  fs.writeFile('src/lib.rs', code, function (err) {
+    if (err) throw err
 
     execute(
       "RUSTFLAGS='-C link-arg=-s' cargo build --release --target wasm32-unknown-unknown",
       function (file) {
-        const buf = fs.readFileSync("target/wasm32-unknown-unknown/release/crypto.wasm");
+        const buf = fs.readFileSync('target/wasm32-unknown-unknown/release/crypto.wasm')
         return res.send({
           fileBuf: buf,
-        });
+        })
       }
-    );
-  });
-});
+    )
+  })
+})
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+  console.log(`Server listening on ${PORT}`)
+})
 
 function execute(command, callback) {
   exec(command, function (error, stdout, stderr) {
-    callback(stdout);
-  });
+    callback(stdout)
+  })
 }
